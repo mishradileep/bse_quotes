991d768516e5d5b55afb56c96353d40fa09b4500
mishradileep <mishradileep@gmail.com>
2013-01-25 00:19:58 +0530

    Pushing changes from Home - 24 Dec Block & Page Completed
    Signed-off-by: mishradileep <mishradileep@gmail.com>

---
+++ theme/block-quote.tpl.php
@@ -0,0 +1,13 @@
+
+<table width="100%" border="1" cellspacing="5" cellpadding="5">
+  <tr>
+    <th>Script Title</th>
+    <th>Quote</th>
+  </tr>
+  <?php foreach($quotes as $quote) { ?>
+  <tr>
+    <td><?php print $quote['Scrip'][0]['scripName']?></td>
+    <td><?php print $quote['Scrip'][0]['last']?></td>
+  </tr>
+  <?php } ?>
+</table>
