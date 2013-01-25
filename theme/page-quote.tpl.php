ed2d9c1aceb5429a8929d6cb23d1ce02f7ca1db4
mishradileep <mishradileep@gmail.com>
2013-01-25 23:36:44 +0530

    Coder module code review changes
    Signed-off-by: mishradileep <mishradileep@gmail.com>

--- theme/page-quote.tpl.php
+++ theme/page-quote.tpl.php
@@ -1,27 +1,29 @@
 <?php foreach($quotes as $quote) { ?>
 <table width="100%" border="1" cellspacing="5" cellpadding="5">
-  
-  <tr>
-    <td colspan="6"><h2><?php print $quote['Scrip'][0]['scripName']?></h2></td>
-  </tr>
-  <tr>
-    <td><strong>Date</strong></td>
-    <td><strong>Open</strong></td>
-    <td><strong>Low</strong></td>
-    <td><strong>Hight</strong></td>
-    <td><strong>Close</strong></td>
-    <td><strong>Last</strong></td>
-  </tr>
-  <?php foreach($quote['Scrip'] as $data) {?>
-  <tr>
-    <td><?php print $data['tradedDate']; ?></td>
-    <td><?php print $data['open']; ?></td>
-    <td><?php print $data['low']; ?></td>
-    <td><?php print $data['high']; ?></td>
-    <td><?php print $data['close']; ?></td>
-    <td><?php print $data['last']; ?></td>
-  </tr>
-  <?php }?>
+
+	<tr>
+		<td colspan="6"><h3>
+				<b><?php print $quote['Scrip'][0]['scripName']?></b> ( <?php print $quote['Scrip'][0]['last']?> )
+			</h3></td>
+	</tr>
+	<tr>
+		<td><strong>Date</strong></td>
+		<td><strong>Open</strong></td>
+		<td><strong>Low</strong></td>
+		<td><strong>Hight</strong></td>
+		<td><strong>Close</strong></td>
+		<td><strong>Last</strong></td>
+	</tr>
+	<?php foreach($quote['Scrip'] as $data) {?>
+	<tr>
+		<td><?php print $data['tradedDate']; ?></td>
+		<td><?php print $data['open']; ?></td>
+		<td><?php print $data['low']; ?></td>
+		<td><?php print $data['high']; ?></td>
+		<td><?php print $data['close']; ?></td>
+		<td><?php print $data['last']; ?></td>
+	</tr>
+	<?php }?>
 
 </table>
-  <?php } ?>
+<?php } ?>
